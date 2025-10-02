cbaa09010237625587e6a4a86450642fd14f1b34
luizodm <luizd.avila@hotmail.com>
2025-10-02 17:48:25 -0300

    first commit

---
+++ src/ThemeContext.tsx
@@ -0,0 +1,32 @@
+import { createContext, useState, type ReactNode, useContext } from "react"
+
+interface ThemeContextType {
+    theme: "light" | "dark"
+    toggleTheme: () => void
+}
+
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined)
+
+export const ThemeProvider = ({ children }: { children: ReactNode }) => {
+    const [theme, setTheme] = useState<"light" | "dark">("light")
+
+    const toggleTheme = () => {
+        setTheme((previousTheme) => (previousTheme === "light" ? "dark" : "light"))
+    }
+
+    return (
+        <ThemeContext.Provider value={{ theme, toggleTheme }}>
+            {children}
+        </ThemeContext.Provider>
+    )
+}
+
+export const useTheme = () => {
+    const context = useContext(ThemeContext);
+
+    if (!context) {
+        throw new Error("useTheme deve ser utilizado com um themeProvider")
+    }
+
+    return context
+}
